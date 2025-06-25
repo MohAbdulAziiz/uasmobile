702b49a760b93e20c06012e088f9766545a4b3a0
MOH. ABDUL AZIZ <148872208+MohAbdulAziiz@users.noreply.github.com>
2025-06-25 21:07:26 +0700

    Update client-provider.jsx

--- src/components/client-provider.jsx
+++ src/components/client-provider.jsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { SessionProvider } from 'next-auth/react'
+import supabase from '@/lib/supabaseClient';
 
 export default function ClientProvider({ children }) {
   return <SessionProvider>{children}</SessionProvider>
